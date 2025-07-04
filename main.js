4c1c9dfdc1154b5ebd106e8e26edb45c69b4480e
Khanh <khanhkhuukhai@gmail.com>
2025-07-04 22:29:38 +0700

    add all files

---
+++ main.js
@@ -0,0 +1,45 @@
+const input_value = document.querySelector(".todo_input_text");
+const add_button = document.querySelector(".todo_input_button");
+const todo_list = document.querySelector(".todo_list ul");
+
+function add_todo(input_value) {
+  if (input_value.trim() === "") {
+    alert("Pls enter a task");
+    return;
+  }
+  const ul = document.querySelector(".todo_list ul");
+  const li = document.createElement("li");
+  const checkbox = document.createElement("input");
+  const delete_button = document.createElement("button");
+  checkbox.type = "checkbox";
+  delete_button.textContent = "Delete";
+  delete_button.classList.add("delete_button");
+  const span = document.createElement("span");
+  span.textContent = input_value;
+  checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+      span.classList.add("todo_item_check");
+    } else {
+      span.classList.remove("todo_item_check");
+    }
+  });
+  delete_button.addEventListener("click", (e) => {
+    const li = e.target.parentElement;
+    li.remove(); // Remove the todo item from the list
+  });
+  li.appendChild(checkbox);
+  li.appendChild(span);
+  li.appendChild(delete_button);
+  ul.appendChild(li);
+}
+add_button.addEventListener("click", () => {
+  add_todo(input_value.value);
+  input_value.value = ""; // Clear the input field after adding the todo
+});
+input_value.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault(); // Prevent the default action of the Enter key
+    add_button.click(); // Trigger the click event on the add button
+  }
+  input_value = ""; // Clear the input field after adding the todo
+});
